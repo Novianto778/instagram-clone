5958cb9f98dd27ff866176675f5cd89c2ab1c735
novianto22 <99942803+novianto22@users.noreply.github.com>
2022-03-23 16:54:06 +0700

    Login UI and Tailwind color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      white: "#ffffff",
+      blue: {
+        medium: "#005c98",
+      },
+      black: {
+        light: "#262626",
+        faded: "#000059",
+      },
+      gray: {
+        base: "#616161",
+        background: "#fafafa",
+        primary: "#dbdbdb",
+      },
+      red: {
+        primary: "#ed4956",
+      },
+    },
   },
   plugins: [],
 };
